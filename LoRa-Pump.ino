59d91f3e3ab739030f97988186b51002fde7024d
Seth Rossman <41768574+SethyRoosman@users.noreply.github.com>
2019-11-14 19:23:16 -0500

    Added OLED display and just a general upgrade
    Read water sensor function was called multiple times which led to varied readings --> assigned to a "static" variable that just changed once per loop

--- LoRa-Pump.ino
+++ LoRa-Pump.ino
@@ -1,4 +1,13 @@
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+
 #include <SoftwareSerial.h>
+#include <Adafruit_SSD1306.h>
+
+// OLED shit
+#define SCREEN_WIDTH 128 // OLED display width, in pixels
+#define SCREEN_HEIGHT 64 // OLED display height, in pixels
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
 
 /* This script essentially turns on the pump via a relay when the water is low enough
  *  We don't need to automate the aerator since it should always be on.
@@ -13,7 +22,7 @@ int moistureAnalog = A0;
 int moisturePower = 8; //5V will burn the sensor thingy so this turns on and off power instead
 int relay_pin = 7;
 
-int waterValue = "Needs to be set";
+int waterValue = 850;
 
 SoftwareSerial mDot(2, 3); //RX & TX
 
@@ -26,6 +35,13 @@ void setup() {
   
   pinMode(moisturePower, OUTPUT);
   digitalWrite(moisturePower, LOW);
+  
+  if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) { 
+    Serial.println("SSD1306 allocation failed");
+    for(;;); // Don't proceed, loop forever
+}
+  delay(2000);
+
 }
 
 int read_water_sensor() {
@@ -37,15 +53,37 @@ int read_water_sensor() {
 }
 
 void loop() {
+  int water_value = read_water_sensor();
+  
+  // Serial Moniter and pump relay control
   Serial.print("Water Level (0-1023): ");
-  Serial.println(read_water_sensor());
-  if (read_water_sensor() == waterValue) {
+  Serial.println(water_value);
+  if (water_value < waterValue) {
     digitalWrite(relay_pin, HIGH);
+    Serial.println("Relay On");
   } else {
     digitalWrite(relay_pin, LOW);
+    Serial.println("Relay Off");
   }
+
+  // OLED Display
+  display.clearDisplay();
+  delay(100);
+  display.setRotation(1);
+  display.setTextSize(4);
+  display.setTextColor(WHITE);
+  display.setCursor(0,0);
+  
+  // SM stands for Soil Moisture
+  display.println("SM:");
+  //display.setCursor(0,3);
+  display.println(water_value);
+  display.display();
+
+  // LoRa Stuff
   String packet = "AT+SEND=";
-  packet += read_water_sensor();
+  packet += water_value;
+  Serial.println(packet);
   mDot.println(packet);
-  delay(60000);
+  delay(30000);
 }
